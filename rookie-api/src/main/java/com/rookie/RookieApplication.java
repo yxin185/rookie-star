3ff11924d803ad8858ee640c3d88a13215616acc
yxin <819782761@qq.com>
2020-03-22 21:18:47 +0800

    Day1-搭建项目框架

---
+++ rookie-api/src/main/java/com/rookie/RookieApplication.java
@@ -0,0 +1,12 @@
+package com.rookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RookieApplication.class, args);
+    }
+}
